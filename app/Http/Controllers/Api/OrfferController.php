2122f20887c016ad9cbb0a1286b6b7d854931f32
Patryk <patryk.d.janus@gmail.com>
2023-06-15 10:47:02 +0200

    add Offer controller

---
+++ app/Http/Controllers/Api/OrfferController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orffer;
+use Illuminate\Http\Request;
+
+class OrfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Orffer  $orffer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Orffer $orffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Orffer  $orffer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Orffer $orffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Orffer  $orffer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Orffer $orffer)
+    {
+        //
+    }
+}
