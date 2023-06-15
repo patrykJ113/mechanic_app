76bf153881639d935242fd89ecbd684e09044651
Patryk <patryk.d.janus@gmail.com>
2023-06-15 10:47:20 +0200

    add models for orders and offers

---
+++ app/Models/Orffer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orffer extends Model
+{
+    use HasFactory;
+}
